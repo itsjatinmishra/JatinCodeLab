ce795aedf926d37b872b55db84ac4ab0469140f2
Jatin Mishra <itsjatinmishra@gmail.com>
2026-03-30 11:24:38 +0530

    11_30_03_2026

--- longest_consecutive_sequence.py
+++ longest_consecutive_sequence.py
@@ -1,19 +1,41 @@
 #Brute Force Approach
+# def longestConsecutive(nums):
+#     longest = 0
+
+#     for num in nums:
+#         current = num
+#         count = 1
+
+#         # check next numbers
+#         while current + 1 in nums:
+#             current += 1
+#             count += 1
+
+#         longest = max(longest, count)
+
+#     return longest
+
+#Optimal Solution
 def longestConsecutive(nums):
-    longest = 0
+    if not nums:
+        return 0
 
-    for num in nums:
-        current = num
-        count = 1
+    num_set = set(nums)
+    longestSub = 0
 
-        # check next numbers
-        while current + 1 in nums:
-            current += 1
-            count += 1
+    for num in num_set:
+        if num - 1 not in num_set:
+            currNum = num
+            currSub = 1
 
-        longest = max(longest, count)
+            while currNum + 1 in num_set:
+                currNum += 1
+                currSub += 1
 
-    return longest
+            longestSub = max(longestSub, currSub)
+
+    return longestSub
+
 
 # test
 nums = [100, 4, 200, 1, 3, 2]
