f65d67ec737f6ad3611858f9ef603e2e1628b4c8
Jatin Mishra <itsjatinmishra@gmail.com>
2026-03-31 12:29:11 +0530

    12_31_03_2026

--- first_missing_positive.py
+++ first_missing_positive.py
@@ -31,7 +31,41 @@ def find_missing_positive(nums):
     return len(nums) + 1
 
 
+
+def firstMissingPositive(nums):
+    n = len(nums)
+
+    # Step 1: Check if 1 is present
+    if 1 not in nums:
+        return 1
+
+    # Step 2: Replace negatives, zeros, and numbers > n with 1
+    for i in range(n):
+        if nums[i] <= 0 or nums[i] > n:
+            nums[i] = 1
+
+    # Step 3: Mark indices
+    for i in range(n):
+        a = abs(nums[i])
+
+        if a == n:
+            nums[0] = -abs(nums[0])
+        else:
+            nums[a] = -abs(nums[a])
+
+    # Step 4: Find first missing positive
+    for i in range(1, n):
+        if nums[i] > 0:
+            return i
+
+    # Step 5: Check index 0
+    if nums[0] > 0:
+        return n
+
+    return n + 1
+
 # Example usage
 nums = [3, 4, -1, 1]
 result = find_missing_positive(nums)
-print(result)
+resultOptimized = firstMissingPositive(nums)
+print(f'This is result by using dictonary method: {result}\nThis is result by using optimized solution: {resultOptimized}')
