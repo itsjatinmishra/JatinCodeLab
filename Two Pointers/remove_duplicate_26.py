61f69e44bc171d100ea397418b0d5b317ad125e7
Jatin Mishra <itsjatinmishra@gmail.com>
2026-04-12 16:07:40 +0530

    remove_duplicate_array_27_12_04_2026

--- Two Pointers/remove_duplicate_26.py
+++ Two Pointers/remove_duplicate_26.py
@@ -1,13 +1,33 @@
 def removeDuplicates(nums):
+    # Pointer to place the next unique element
     insertIndex = 1
 
+    # Loop starts from index 1 (second element)
+    # because we compare current element with previous one
     for i in range(1, len(nums)):
-        if nums[i-1] != nums[i]:
+
+        # If current element is different from previous
+        # it means it's a new unique element
+        if nums[i - 1] != nums[i]:
+
+            # Place the unique element at insertIndex position
             nums[insertIndex] = nums[i]
+
+            # Move insertIndex forward for next unique element
             insertIndex += 1
     
+    # Return the count of unique elements
     return insertIndex
 
 
+# Input array (must be sorted)
 nums = [0,0,1,1,1,2,2,3,3,4]
-print(removeDuplicates(nums))
+
+# Function call
+result = removeDuplicates(nums)
+
+# Print number of unique elements
+print(result)
+
+# Optional: Print updated array (only first 'result' elements are valid)
+print(nums[:result])
