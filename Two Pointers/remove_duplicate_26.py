10e331b532d8878502fcc9b01c22dcddb8b4d112
Jatin Mishra <itsjatinmishra@gmail.com>
2026-04-12 16:06:24 +0530

    remove_duplicate_array_27_12_04_2026

---
+++ Two Pointers/remove_duplicate_26.py
@@ -0,0 +1,13 @@
+def removeDuplicates(nums):
+    insertIndex = 1
+
+    for i in range(1, len(nums)):
+        if nums[i-1] != nums[i]:
+            nums[insertIndex] = nums[i]
+            insertIndex += 1
+    
+    return insertIndex
+
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+print(removeDuplicates(nums))
