4630c005d5392845e63e360d883a9a0b4ede2f97
Jatin Mishra <itsjatinmishra@gmail.com>
2026-04-13 20:42:36 +0530

    third_max_number_update_short_code_28_12_04_2026

--- Array/third_max_num.py
+++ Array/third_max_num.py
@@ -1,26 +1,37 @@
+# def thirdMax(nums):
+#     first = second = third = float('-inf')
+
+#     for num in nums:
+#         # ignore duplicates
+#         if num == first or num == second or num == third:
+#             continue
+
+#         if num > first:
+#             third = second
+#             second = first
+#             first = num
+
+#         elif num > second:
+#             third = second
+#             second = num
+
+#         elif num > third:
+#             third = num
+
+#     if third == float('-inf'):
+#         return first
+#     return third
+
+
+### More easy code solution
+
 def thirdMax(nums):
-    first = second = third = float('-inf')
+    nums = list(set(nums))   # remove duplicates
+    nums.sort(reverse=True)  # sort descending
 
-    for num in nums:
-        # ignore duplicates
-        if num == first or num == second or num == third:
-            continue
-
-        if num > first:
-            third = second
-            second = first
-            first = num
-
-        elif num > second:
-            third = second
-            second = num
-
-        elif num > third:
-            third = num
-
-    if third == float('-inf'):
-        return first
-    return third
+    if len(nums) < 3:
+        return nums[0]
+    return nums[2]
 
 
 nums = [2, 2, 3, 1]
