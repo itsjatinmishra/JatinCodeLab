62a455fb8394339c66b098034dd55e9a1da41483
Jatin Mishra <itsjatinmishra@gmail.com>
2026-04-17 17:56:46 +0530

    two_pointer_updated_32_17_04_2026

--- Two Pointers/next_permutation.py
+++ Two Pointers/next_permutation.py
@@ -1,26 +1,38 @@
 def nextPermutation(nums):
+    # Step 1: Start from second last index
+    # We move from right → left to find the "break point"
     i = len(nums) - 2
 
-    # find breakpoint
+    # Step 2: Find the first element which is smaller than its next element
+    # This is where the next permutation change should happen
     while i >= 0 and nums[i] >= nums[i + 1]:
         i -= 1
 
-    # swap with next greater
+    # Step 3: If such an element is found (i >= 0)
+    # we need to replace it with the just bigger element from right side
     if i >= 0:
         j = len(nums) - 1
+
+        # Find the smallest number greater than nums[i] from right side
         while nums[j] <= nums[i]:
             j -= 1
+
+        # Swap both values
         swap(nums, i, j)
 
-    # reverse suffix
+    # Step 4: Reverse the part after index i
+    # This makes the right side smallest (ascending order)
     reverse(nums, i + 1)
 
 
 def swap(nums, i, j):
+    # Swap two elements using Python tuple unpacking
     nums[i], nums[j] = nums[j], nums[i]
 
 
 def reverse(nums, i):
+    # Reverse the subarray from index i to end
+    # This converts decreasing order → increasing order
     nums[i:] = reversed(nums[i:])
 
 
