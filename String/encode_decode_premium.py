28fb73d1bc744ada2e4a1a7614bbfa23062b8ffa
Jatin Mishra <itsjatinmishra@gmail.com>
2026-04-19 10:49:28 +0530

    string_leetcode_premium_problem_solution_updated__35_19_04_2026

--- String/encode_decode_premium.py
+++ String/encode_decode_premium.py
@@ -1,23 +1,36 @@
 def encode(strs):
+    # This function takes a list of strings and joins them into one string
+    # using '#' as a separator
     encoded_str = ""
-    for i in strs:
-        encoded_str += i + "#"
+    
+    for s in strs:
+        encoded_str += s + "#"   # Add each string followed by '#'
     
     return encoded_str
 
+
 def decode(encoded_str):
+    # This function converts the encoded string back into a list of strings
     result = []
     element = ""
-    for i in range(len(encoded_str)):
-        if encoded_str[i] != "#":
-            element += encoded_str[i]
+    
+    for char in encoded_str:
+        if char != "#":
+            element += char   # Build the current word character by character
         else:
-            result.append(element)
-            element = ""
+            result.append(element)  # Word ends when '#' is found
+            element = ""            # Reset for next word
+    
     return result
 
-strs = ["ab", "b", "c", "abc"]
-encoded_str = encode(strs)
-print(encoded_str)
 
-print(decode(encoded_str))
+# Input list
+strs = ["ab", "b", "c", "abc"]
+
+# Encode the list into a single string
+encoded_str = encode(strs)
+print("Encoded:", encoded_str)
+
+# Decode the string back into list
+decoded_list = decode(encoded_str)
+print("Decoded:", decoded_list)
