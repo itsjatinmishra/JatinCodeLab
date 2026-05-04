bb000420841d79536a817fa4c2ad01c390b96754
Jatin Mishra <itsjatinmishra@gmail.com>
2026-05-04 13:26:44 +0530

    Searching_51_04_04_2026

--- Searching Sorting/binary_search.py
+++ Searching Sorting/binary_search.py
@@ -1,21 +1,20 @@
 def binary_search(nums, target):
-    start = 0
-    end = len(nums) - 1
+    start, end = 0, len(nums) - 1
 
     while start <= end:
-        mid = (start + end) // 2
+        mid = (start + end) // 2  # find middle index
 
         if nums[mid] == target:
-            return mid
+            return mid            # target found
         elif nums[mid] > target:
-            end = mid - 1
-        elif nums[mid] < target:
-            start = mid + 1
-    
-    return -1
+            end = mid - 1         # search left
+        else:
+            start = mid + 1       # search right
+
+    return -1                     # target not found
 
 
-nums = [-1,0,3,5,9,12]
+nums = [-1, 0, 3, 5, 9, 12]
 target = 9
 
 print(binary_search(nums, target))
