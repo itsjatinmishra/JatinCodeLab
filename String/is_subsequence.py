f87d25047e41b21bee3e99fc91051bd3bae345d4
Jatin Mishra <itsjatinmishra@gmail.com>
2026-05-12 21:13:14 +0530

    String_Two_Pointer_12-05-2026

---
+++ String/is_subsequence.py
@@ -0,0 +1,19 @@
+def isSubsequence(s, t):
+
+    i = 0
+    j = 0
+
+    while i < len(s) and j < len(t):
+
+        if s[i] == t[j]:
+            i += 1
+
+        j += 1
+
+    return i == len(s)
+
+
+s = "abc"
+t = "ahbgdc"
+
+print(isSubsequence(s, t))
